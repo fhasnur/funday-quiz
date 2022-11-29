e3f8de295d69a2a1993e4d45b53b859491626d18
fhasnur <fandi.hasnur10@gmail.com>
2022-11-30 03:25:45 +0800

    add: score rangking

--- src/Modal.js
+++ src/Modal.js
@@ -3,6 +3,7 @@ import { useGlobalContext } from "./context";
 
 const Modal = () => {
   const { isModalOpen, closeModal, correct, questions } = useGlobalContext();
+
   return (
     <div
       className={`${
@@ -15,6 +16,30 @@ const Modal = () => {
           You answered {((correct / questions.length) * 100).toFixed(0)}% of
           questions correctly
         </p>
+        <p>Rangking</p>
+        <table>
+          <tr>
+            <th>No#</th>
+            <th>Name</th>
+            <th>Score</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Fandi</td>
+            <td>{((correct / questions.length) * 100).toFixed(0)}%</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Agung</td>
+            <td>20%</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Alam</td>
+            <td>10%</td>
+          </tr>
+        </table>
+
         <button className="close-btn" onClick={closeModal}>
           Play again
         </button>
